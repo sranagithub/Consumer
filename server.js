1ad4625f4e28060f49f02b00183231a9f3eddaa3
Saurabh <sau_rana@hotmail.com>
2017-01-01 22:00:34 +0530

    This is modified by some user

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ var express=require('express');
 var app=express();
 
 app.get('/', function(req, resp){
-    resp.send("This is a response from server, modified");
+    resp.send("This is a response from server, modified,cloned");
     resp.end;
-})
+});
+
+app.get('/', function(req, resp){
+    resp.send("This is a response from server, modified,cloned");
+    resp.end;
+});
+
 app.listen(3000, function(){
     console.log('server started at 3000.');
 })
